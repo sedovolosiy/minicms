0a7f06e7579205fef406596859b031d2db598bb7
Siedovolosij Serhij <sedovolosiy@gmail.com>
2016-02-25 18:00:16 +0200

    commit

--- tpl/header.php
+++ tpl/header.php
@@ -12,7 +12,7 @@
 			<div id="left">
 				<div id="logo">
 					<div class="name">AVTO</div>
-					<div class="tag">Лучшие автомобили для Вас</div>
+					<div class="tag">Лучшие автомобили для Вас!</div>
 				</div>
 			</div>
 			<div id="car"></div>
